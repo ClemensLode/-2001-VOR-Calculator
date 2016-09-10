fe0b6aa464e5065a5ad7ee2eb2a4192cd4490a9a
Clemens Lode <contact@lode.de>
2016-09-10 15:36:51 +0200

    v2

--- vorcalc.cpp
+++ vorcalc.cpp
@@ -3,10 +3,28 @@
 #include <conio.h>
 #include <math.h>
 #include <windows.h>
-
+#include <iostream.h>
+#include <fstream.h>
 #include <time.h>
 
-#define RESTART 1
+
+#define RUNNINGS 10
+
+
+/*noch einfuegen: mehrere Laeufe hintereinander, ergebnisse+loesungen zusammenspeichern und beste finden und in Datei umgeben
+(thema: user friendlyness)
+
+dann:
+andere rassen einfuegen
+
+~ Nahkampf*/
+
+
+
+
+// reading binary file
+
+#define RESTART 0
 // 0: Initialize all random
 // 1: Initialize all 0
 // 2: Initialize all with 1 Std Infantry Troup
@@ -14,7 +32,7 @@
 #define MUTATIONS 100
 #define MUTATION_FAKTOR 500
 
-#define MAX_UNITS 20
+#define MAX_UNITS 25
 
 #define EMPTY 0
 #define COMMANDER 1
@@ -51,7 +69,6 @@
 #define DART 10
 
 
-#define PTS 1000
 
 #define MAX_PLAYER 200
 
@@ -59,6 +76,27 @@
 #define WEAPON_TYPES 11
 
 #define REPRODUCTION 20
+
+#define MAX_GENERATIONS 50
+
+unsigned char run,y,rfit;
+unsigned short afit;
+unsigned short PTS;
+
+struct save
+{
+	struct Unit
+	{
+		unsigned char type;
+		unsigned char S;//Special Abilities and Armament
+	} unit[MAX_UNITS];
+	unsigned short fitness;
+	unsigned short PTs;
+	unsigned char force[TROUP_TYPES];
+	unsigned char weaps[TROUP_TYPES][WEAPON_TYPES];
+} Save[RUNNINGS];
+
+
 
 struct s_Name
 {
@@ -82,23 +120,12 @@ struct s_EStatistics
 {
 	unsigned char life,size,armor,body,count;
 };
-#define ENEMY_TYPES 5
-const s_EStatistics Estats[ENEMY_TYPES]=
-{
-	{4,LARGE,7,7,1}, //Haeuptling
-	{3,LARGE,5,6,3}, // Razorback Bulle
-	{3,LARGE,3,5,1}, // Momma
-	{2,LARGE,3,5,5}, // Eindorne
-	{1,SMALL,1,3,10} // Junge
-
-	/*{2,MEDIUM,2,4,4}, //Zykhee Krieger
-	{1,MEDIUM,0,3,1}*/
-};
+#define ENEMY_TYPES 10
+s_EStatistics Estats[ENEMY_TYPES];
+unsigned short enemy_count,generation;
 
 
-
-
-const ENEMY_FORCE=Estats[0].life*Estats[0].count+Estats[1].life*Estats[1].count+Estats[2].life*Estats[2].count+Estats[3].life*Estats[3].count+Estats[4].life*Estats[4].count;
+unsigned char num;
 
 
 struct s_TStatistics
@@ -154,7 +181,7 @@ const s_WStatistics Wstats[WEAPON_TYPES]=
 
 unsigned char x,i,j,A,B,tempb;
 unsigned short tempw;
-
+long n;
 
 class Player
 {
@@ -162,7 +189,6 @@ public:
 
 	unsigned char force[TROUP_TYPES];
 	unsigned char weaps[TROUP_TYPES][WEAPON_TYPES];
-	unsigned short st;
 
 	struct Unit
 	{
@@ -177,15 +203,14 @@ public:
 
 	void ChooseTarget()
 	{
-		for(x=0;x<ENEMY_TYPES-1;x++)
-			if(rand()%ENEMY_FORCE<(Estats[x].count*Estats[x].life))
+		for(x=0;x<num-1;x++)
+			if(rand()%enemy_count<(Estats[x].count*Estats[x].life))
 				target=x;
-			else target=ENEMY_TYPES-1;
+			else target=num-1;
 	}
 
 	unsigned char Allowed() //0 => death, everything else => PTS
 	{
-		st=0;
 		// Zu brutal ...
 		for(i=0;i<TROUP_TYPES;i++)
 		{
@@ -211,7 +236,6 @@ public:
 					weaps[unit[i].type][unit[i].S]++;
 				}
 				force[unit[i].type]++;
-					
 			}
 		}
 	
@@ -219,48 +243,38 @@ public:
 		if(PTs>PTS)
 			return(0);
 		
-//StrafPunkte
-
 		for(i=1;i<WEAPON_TYPES;i++)
 		{
 			if(weaps[COMMANDER][i]>0)
-				st++;
-				//return(0);
+				return(0);
 			if(weaps[SERGEANT][i]>0)
-				st++;
-				//return(0);
+				return(0);
 		}
 
 		for(i=1;i<WEAPON_TYPES;i++)
 			if((i!=SNIPER)&&(weaps[SCOUTINFANTRY][i]>0))
-				st++;
-				//return(0);
+				return(0);
 
 		if(weaps[SCOUTINFANTRY][SNIPER]>force[SCOUTINFANTRY]/2)
-			st++;
-			//return(0);
+			return(0);
 		
 		if( (weaps[STDINFANTRY][SNIPER]>0) || (weaps[ELITEINFANTRY][SNIPER]>0))
-			st++;
-			//return(0);
+			return(0);
 
 		for(i=HARBINGER;i<WEAPON_TYPES;i++)
 			if( (weaps[STDINFANTRY][i]>0) || (weaps[ELITEINFANTRY][i]>0) || (weaps[SCOUTINFANTRY][i]>0) || (weaps[SPECIALFORCE][i]>0))
-				st+=2;
-				//return(0);
+				return(0);
 		
 		
 
 		if( (weaps[STDINFANTRY][BULLDOG]>force[STDINFANTRY]/4) || (weaps[STDINFANTRY][ROTTWEILER]+weaps[STDINFANTRY][DRACO]>force[STDINFANTRY]/4) ||
 			(weaps[ELITEINFANTRY][BULLDOG]>force[ELITEINFANTRY]/2) || (weaps[ELITEINFANTRY][ROTTWEILER]+weaps[ELITEINFANTRY][DRACO]>force[ELITEINFANTRY]/4))
-			st+=2;
-			//return(0);
+			return(0);
 
 		// Vehicles
 		if( (force[ARES]+force[WENDIGO]+force[FENRIS]+force[ODIN]+force[LOKI]>(PTS/1000)+1))
-			st+=4;
-			//return(0);
-/*		for(i=0;i<HARBINGER;i++)
+			return(0);
+		for(i=0;i<HARBINGER;i++)
 			if(weaps[ARES][i]>0)
 				return(0);
 		if(weaps[ARES][APE]>0)
@@ -272,22 +286,18 @@ public:
 					return(0); //Multiple Weapons*/
 
 		if( ((force[STDINFANTRY]>0)&&(force[STDINFANTRY]<4)) || ((force[ELITEINFANTRY]>0)&&(force[ELITEINFANTRY]<4)) || (force[SCOUTINFANTRY]==1))
-			st++;
-			//return(0);
+			return(0);
 
 		//Sergeants
 		if( (force[SERGEANT]<((force[STDINFANTRY]+7)/8)+((force[ELITEINFANTRY]+7)/8)/*+(force[BRAWLERINFANTRY]/8)+(force[ARMOREDASSAULT]/8)*/)||
-			(force[SERGEANT]>((force[STDINFANTRY]+3)/4)+((force[ELITEINFANTRY]+3)/4)/*+(force[BRAWLERINFANTRY]/4)+(force[ARMOREDASSAULT]/6)*/))
-			st+=2;
-			//return(0);
+			(force[SERGEANT]>(force[STDINFANTRY]/4+force[ELITEINFANTRY]/4)/*+(force[BRAWLERINFANTRY]/4)+(force[ARMOREDASSAULT]/6)*/))
+			return(0);
 
 		if(force[STDINFANTRY]+force[ELITEINFANTRY]>force[SERGEANT]*8)
-			st++;
-			//return(0);
+			return(0);
 
 		if(force[STDINFANTRY]+force[ELITEINFANTRY]<force[SERGEANT]*4)
-			st++;
-			//return(0);
+			return(0);
 		//<> Infantry Troups
 
 		if( ((force[ELITEINFANTRY]+7)/8>force[STDINFANTRY]/4) || /*(force[BRAWLERINFANTRY]>force[STDINFANTRY]) || (force[HELLHOUNDINFANTRY]>force[STDINFANTRY]) ||*/
@@ -295,28 +305,27 @@ public:
 			((force[ATDDRONE]+2)/3>force[SERGEANT]/*Brawler, armored abziehen!*/) || 
 			(force[HTDDRONE]*2>force[SERGEANT]) || (force[SUPPLYDRONE]>force[SERGEANT]+force[SCOUTINFANTRY]/2 /*brawler, armored...*/)
 			)
-			st++;
-			//return(0);
+			return(0);
 
 
 //		 Special Forces
 		if( 
 			(force[SPECIALFORCE]>8) || ((force[SPECIALFORCE]<4)&&(force[SPECIALFORCE]>0)) )
-			st++;
-			//return(0);
+			return(0);
 
 		//Commander
 		if(force[COMMANDER]>1)
-			st++;
-			//return(0);
+			return(0);
 
-		if(force[SUPPLYDRONE]*4>=force[SPECIALFORCE]+force[STDINFANTRY]+force[ELITEINFANTRY]+force[SCOUTINFANTRY])
-			Supply=1;
-		else Supply=0;
+		Supply=force[SUPPLYDRONE]*3;
+//		if(force[SUPPLYDRONE]*4>=force[SPECIALFORCE]+force[STDINFANTRY]+force[ELITEINFANTRY]+force[SCOUTINFANTRY])
+//			Supply=1;
+//		else Supply=0;
 
 		return(1);
 	}
-	
+
+
 	void Calculate()
 	{
 		fitness=0;
@@ -329,7 +338,7 @@ public:
 					
 					(
 					(Supply==0)*(Tstats[unit[i].type].RC+Wstats[unit[i].S].RC+Estats[target].size-2+Wstats[unit[i].S].AIMING)
-				   +(Supply==1)*
+				   +(Supply>0)*
 			  (10-
 			    (10-(Tstats[unit[i].type].RC+Wstats[unit[i].S].RC+Estats[target].size-1+Wstats[unit[i].S].AIMING))*			
 				(10-(Tstats[unit[i].type].RC+Wstats[unit[i].S].RC+Estats[target].size-1+Wstats[unit[i].S].AIMING))/10
@@ -348,13 +357,14 @@ public:
 					*(Wstats[unit[i].S].USES)
 					*(Wstats[unit[i].S].AREA+1)
 					*(1+Wstats[unit[i].S].AUTO*Estats[target].size);
+					if(Supply>0) Supply--;
 			}
 			else if(unit[i].type==SPECIALFORCE)
 			{
 				fitness+=
 					(			
 					(Supply==0)*(Tstats[unit[i].type].RC+Wstats[unit[i].S].RC+Estats[target].size-1+(unit[i].S==SNIPER))
-					+(Supply==1)
+					+(Supply>1)
 					*(10-( (unit[i].S!=SNIPER)*
 					 (10-(Tstats[unit[i].type].RC+Wstats[unit[i].S].RC+Estats[target].size))
 					*(10-(Tstats[unit[i].type].RC+Wstats[unit[i].S].RC+Estats[target].size))/10)))
@@ -372,6 +382,7 @@ public:
 					*(Wstats[unit[i].S].USES)
 					*(Wstats[unit[i].S].AREA+1)
 					*(1+Wstats[unit[i].S].AUTO*Estats[target].size);
+					if((Supply>0)&&(unit[i].S!=SNIPER)) Supply--;
 			}			
 
 
@@ -608,14 +619,14 @@ else
 	if(rand()%(MAX_UNITS)==0) return(SUPPLYDRONE);
 else 
 	if(rand()%(MAX_UNITS*2)==0) return(ARES);
-else 
-	if(rand()%(MAX_UNITS*2)==0) return(WENDIGO);
-else
-	if(rand()%(MAX_UNITS*2)==0) return(FENRIS);
-else 
-	if(rand()%(MAX_UNITS*2)==0) return(ODIN);
-else 
-	if(rand()%(MAX_UNITS*2)==0) return(LOKI);
+//else 
+//	if(rand()%(MAX_UNITS*2)==0) return(WENDIGO);
+//else
+//	if(rand()%(MAX_UNITS*2)==0) return(FENRIS);
+//else 
+//	if(rand()%(MAX_UNITS*2)==0) return(ODIN);
+//else 
+//	if(rand()%(MAX_UNITS*2)==0) return(LOKI);
 else 
 	return(EMPTY);
 }
@@ -651,6 +662,49 @@ void ChooseWeapon(unsigned char w)
 	}
 };
 
+void Mutate()
+{
+for(j=0;j<MUTATIONS;j++)
+	{
+		if((rand()%MUTATION_FAKTOR==0))
+		{
+			x=rand()%MAX_UNITS;
+			unit[x].type=ChooseGuy();
+			ChooseWeapon(x);
+		if((rand()%MUTATION_FAKTOR==0))
+			ChooseWeapon(rand()%MAX_UNITS);
+		}
+	}
+}
+
+void Init()
+{
+	fitness=0;
+	if(RESTART==2)
+	{
+		unit[0].type=STDINFANTRY;
+		unit[0].S=PITBULL;
+		unit[1].type=STDINFANTRY;
+		unit[1].S=PITBULL;
+		unit[2].type=STDINFANTRY;
+		unit[2].S=PITBULL;
+		unit[3].type=STDINFANTRY;
+		unit[3].S=BULLDOG;
+		unit[4].type=SERGEANT;
+		unit[4].S=PITBULL;
+	}
+	else
+	for(j=0;j<MAX_UNITS;j++)
+	{
+		if(RESTART==1)
+			unit[j].type=0;
+			else 
+			unit[j].type=ChooseGuy();
+		
+		ChooseWeapon(j);
+	}
+}
+
 } player[MAX_PLAYER];
 
 
@@ -658,57 +712,77 @@ void ChooseWeapon(unsigned char w)
 
 int main(int argc, char* argv[])
 {
+  const char * filename = "enemy.army";
+  unsigned char pos,calc;
+  unsigned char * buffer;
+  long size;
+  generation=0;
+  ifstream file (filename, ios::in|ios::binary|ios::ate);
+  size = file.tellg();
+  file.seekg (0, ios::beg);
+  buffer = new unsigned char [size];
+  file.read (buffer, size);
+  file.close();
+
+
+  //# LIFE SIZE ARMOR BODY NUMBER
+	n=1;
+	num=0;
+	pos=0;
+	PTS=(buffer[0]-48)*250+250;
+	while(n<size)
+	{
+		if((buffer[n]>=48)&&(buffer[n]<58))
+		{
+			if(pos>4)
+			{
+				pos=0;
+				num++;
+			}
+			switch(pos)
+			{
+				case 0:Estats[num].life=buffer[n]-48;pos++;break;
+				case 1:Estats[num].size=buffer[n]-48;pos++;break;
+				case 2:Estats[num].armor=buffer[n]-48;pos++;break;
+				case 3:Estats[num].body=buffer[n]-48;pos++;break;
+				case 4:Estats[num].count=buffer[n]-48;pos++;break;
+			}
+		}
+		n++;
+	}
+	for(i=0;i<num;i++)
+		enemy_count+=Estats[i].life*Estats[i].count;
+ 
+
 	srand(time(NULL));
 	 HANDLE hStdOut=GetStdHandle(STD_OUTPUT_HANDLE);
 	 COORD coord={0,0};
+	COORD coord2={25,22};
+	SetConsoleCursorPosition(hStdOut,coord2);
+	printf("This is Freeware. Brought to you by clawsoftware.de");
 
-	
 	for(i=0;i<MAX_PLAYER;i++)
-	{
-		player[i].fitness=0;
-		if(RESTART==2)
-		{
-			player[i].unit[0].type=STDINFANTRY;
-			player[i].unit[0].S=PITBULL;
-			player[i].unit[1].type=STDINFANTRY;
-			player[i].unit[1].S=PITBULL;
-			player[i].unit[2].type=STDINFANTRY;
-			player[i].unit[2].S=PITBULL;
-			player[i].unit[3].type=STDINFANTRY;
-			player[i].unit[3].S=BULLDOG;
-			player[i].unit[4].type=SERGEANT;
-			player[i].unit[4].S=PITBULL;
-		}
-		else
-		for(j=0;j<MAX_UNITS;j++)
-		{
-			if(RESTART==1)
-				player[i].unit[j].type=0;
-			else 
-				player[i].unit[j].type=player[i].ChooseGuy();
-			
-			player[i].ChooseWeapon(j);
-		}
-		player[i].unit[0].type=FENRIS;
-		
-	}
+		player[i].Init();
+	run=0;
 
-	while(kbhit()==0)
+	while( (kbhit()==0) && (run<RUNNINGS))
 	{
+		for(i=0;i<MAX_PLAYER;i++)
+			player[i].Mutate();
+		
 		for(j=0;j<MAX_PLAYER;j++)
 		{
-			
-			player[j].fitness=0;
 			if(player[j].Allowed()==0)
 			{
+				
 				player[j].PTs=0;
+				player[j].fitness=0;
 				player[j].tod=1;
 			}
 			else
 			{
 				player[j].tod=0;
 				player[j].Calculate();
-				player[j].fitness-=player[j].st*10;
 			};
 		};
 		for(i=0;i<MAX_PLAYER-1;i++)
@@ -743,7 +817,8 @@ int main(int argc, char* argv[])
 				B=A;
 				while((player[B].tod>0)&&(B<MAX_PLAYER-1)) B++;
 				if(player[B].fitness==0) B=A;
-				
+
+				if((player[A].fitness>0)&&(player[B].fitness>0))
 				for(i=0;i<REPRODUCTION;i++)
 					for(x=0;x<MAX_UNITS;x++)
 					{
@@ -755,92 +830,116 @@ int main(int argc, char* argv[])
 						player[MAX_PLAYER-i*2].unit[x].S=player[B].unit[x].S;
 						player[MAX_PLAYER-i*2].tod=0;
 					}
-		SetConsoleCursorPosition(hStdOut,coord);
-		printf("Fit: %i, PTs: %i        \n",player[0].fitness,player[0].PTs);
-
-		for(i=0;i<TROUP_TYPES;i++)
-			printf("%i x %s            \n",player[0].force[i],nameT[i].Name);
-			
-		for(i=0;i<MAX_PLAYER;i++)
+		
+		if(player[0].fitness>0)
 		{
-			if(player[i].tod==1)
-			{
-				player[i].fitness=0;
-//				for(j=0;j<MAX_UNITS;j++)
-//				{
-//					player[i].unit[j].type=rand()%TROUP_TYPES;
-//					player[i].ChooseWeapon(j);
-//				}
-			}
-
-			for(j=0;j<MUTATIONS;j++)
-			{
-				if((rand()%MUTATION_FAKTOR==0))
-				{
-					x=rand()%MAX_UNITS;
-					player[i].unit[x].type=player[i].ChooseGuy();
-					player[i].ChooseWeapon(x);
-				if((rand()%MUTATION_FAKTOR==0))
-				{
-					x=rand()%MAX_UNITS;
-					player[i].ChooseWeapon(x);
-				}
-			}
+			generation++;
+			rfit++;
 		}
-		
-		
-/*		SetConsoleCursorPosition(hStdOut,coord);
-//	for(x=0;x<5;x++)
-//		{
-			printf("Fit: %i, PTs: %i        \n",player[0].fitness,player[0].PTs);
 
-for(i=0;i<TROUP_TYPES;i++)
-//if(player[0].force[i]>0)
-{
-	printf("%i x %s            \n",player[0].force[i],nameT[i].Name);
-	if((i<ATDDRONE)||(i>HTDDRONE))
-	{
-//		for(j=0;j<WEAPON_TYPES;j++)
-//	if(player[0].weaps[i][j]>0)
-//				printf(" %i x %s,",player[0].weaps[i][j],nameWs[j].Name);
-//		printf("                    \n");
-	}
-}
-			
-/*			for(i=0;i<TROUP_TYPES;i++)
-			if(player[x*MAX_PLAYER/6].force[i]>0)
+		SetConsoleCursorPosition(hStdOut,coord);
+
+			calc++;
+			if(calc>2)
+				calc=1;
+		printf("Calculating ... %c\n",calc);
+		printf("%i runs remaining . . .  \n",10-run);
+		printf("Some Debug Output:\n");
+		printf("Fit: %i, PTs: %i    Generation: %i               \n",player[0].fitness,player[0].PTs,generation);
+
+		for(i=1;i<TROUP_TYPES;i++)
+			printf("%i x %s            \n",player[0].force[i],nameT[i].Name);
+		
+		
+		if(player[0].fitness>afit)
+		{
+			afit=player[0].fitness;
+			rfit=0;
+		}
+				
+		
+		if(rfit>MAX_GENERATIONS)
+		{
+			afit=0;
+			rfit=0;
+			generation=0;
+			Save[run].fitness=player[0].fitness;
+			Save[run].PTs=player[0].PTs;
+			for(x=0;x<MAX_UNITS;x++)
 			{
-				printf("%ix%s ",player[x*MAX_PLAYER/6].force[i],nameTs[i].Name);
-				if((i<ATDDRONE)||(i>HTDDRONE))
-				{
-					for(j=0;j<WEAPON_TYPES;j++)
-						if(player[x*MAX_PLAYER/6].weaps[i][j]>0)
-						printf("(%ix%s)",player[x*MAX_PLAYER/6].weaps[i][j],nameWs[j].Name);
-				}
+				Save[run].unit[x].type=player[0].unit[x].type;
+				Save[run].unit[x].S=player[0].unit[x].S;
 			}
-			printf("\n");
-		}*/
+			for(i=0;i<TROUP_TYPES;i++)
+			{
+				Save[run].force[i]=player[0].force[i];
+				for(j=0;j<WEAPON_TYPES;j++)
+					Save[run].weaps[i][j]=player[0].weaps[i][j];
+			}
+			run++;
+			for(i=0;i<MAX_PLAYER;i++)
+				player[i].Init();
 		}
 
 }
 
-for(i=0;i<TROUP_TYPES;i++)
-if(player[0].force[i]>0)
-{
-	printf("%i x %s (%i pts)   \n",player[0].force[i],nameT[i].Name,player[0].force[i]*Tstats[i].Cost);
-	if((i<ATDDRONE)||(i>HTDDRONE))
-	{
-		for(j=0;j<WEAPON_TYPES;j++)
-			if(player[0].weaps[i][j]>0)
-				printf("  o %i x %s (+%i pts)",player[0].weaps[i][j],nameW[j].Name,player[0].weaps[i][j]*Wstats[j].Cost);
-		printf("\n");
-	}
-}
+
+for(i=0;i<run;i++)
+	for(j=0;j<run-i+2;j++)
+		if(Save[j].fitness<Save[j+1].fitness)
+		{
+			tempw=Save[j].fitness;
+			Save[j].fitness=Save[j+1].fitness;
+			Save[j+1].fitness=tempw;
+			tempw=Save[j].PTs;
+			Save[j].PTs=Save[j+1].PTs;
+			Save[j+1].PTs=tempw;
+			for(x=0;x<MAX_UNITS;x++)
+			{
+				tempb=Save[j].unit[x].type;
+				Save[j].unit[x].type=Save[j+1].unit[x].type;
+				Save[j+1].unit[x].type=tempb;
+				tempb=Save[j].unit[x].S;
+				Save[j].unit[x].S=Save[j+1].unit[x].S;
+				Save[j+1].unit[x].S=tempb;
+			}
+			for(x=0;x<TROUP_TYPES;x++)
+			{
+				tempb=Save[j].force[x];
+				Save[j].force[x]=Save[j+1].force[x];
+				Save[j+1].force[x]=tempb;
+				for(y=0;y<WEAPON_TYPES;y++)
+				{
+					tempb=Save[j].weaps[x][y];
+					Save[j].weaps[x][y]=Save[j+1].weaps[x][y];
+					Save[j+1].weaps[x][y]=tempb;
+				}
+			}
+		}			
+
+SetConsoleCursorPosition(hStdOut,coord);
+
+	printf("Calculating completed.														 \n",calc);
+	printf("Final fitness: %i (%i PTs used)                  \n",Save[0].fitness,Save[0].PTs);
+	printf("  UNION Counter - Force:						            				 \n");
 	
+for(i=1;i<TROUP_TYPES;i++)
+	if(Save[0].force[i]>0)
+	{
+		printf("%i x %s (%i pts)                              \n",Save[0].force[i],nameT[i].Name,Save[0].force[i]*Tstats[i].Cost);
+		if((i<ATDDRONE)||(i==ARES))
+		{
+			for(j=0;j<WEAPON_TYPES;j++)
+				if(Save[0].weaps[i][j]>0)
+					printf("  o %i x %s (+%i pts)",Save[0].weaps[i][j],nameW[j].Name,Save[0].weaps[i][j]*Wstats[j].Cost);
+			printf("\n");
+		}  
+	}
 
-
+	printf("																			 \n");
+	delete[] buffer;
 	x=getch();
-
+	x=getch();
 	return 0;
 }
 
